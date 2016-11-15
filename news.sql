b356cd2282626475b7baa07ff27bd5705fa5a1c9
helongquan <791447771@qq.com>
2016-11-15 22:25:57 +0800

    news
    this is a news system about PHP

---
+++ news.sql
@@ -0,0 +1,44 @@
+SET table_type=InnoDB;
+show variables like 'table_type';
+SET character_set_client='utf8';
+SET character_set_connection='utf8';
+SET character_set_database='utf8';
+SET character_set_results='utf8';
+SET character_set_server='utf8';
+SET collation_connection=utf8_chinese_ci;
+SET collation_database=utf8_chinese_ci;
+SET collation_server=utf8_chinese_ci;
+show variables like	'character%';
+show variables like	'collation%';
+create database news;
+use news;
+create table category(
+category_id int auto_increment primary key,
+name char(20) not NULL
+);
+create table users(
+user_id int auto_increment primary key,
+name char(20) not NULL,
+password char(32)
+);
+create table news(
+news_id int auto_increment primary key,
+user_id int,
+category_id int,
+title char(100) not null,
+content text,
+publish_time datetime,
+clicked int,
+attachment char(100),
+constraint FK_news_user foreign key (user_id) references users(user_id),
+constraint FK_news_category foreign key (category_id) references category(category_id)
+);
+create table review(
+review_id int auto_increment primary key,
+news_id int,
+content text,
+publish_time datetime,
+state char(10),
+ip char(15),
+constraint FK_review_news foreign key (news_id) references news(news_id)
+);
---
+++ news_add.php
@@ -0,0 +1,22 @@
+<form method="post" action="news_save.php" enctype="multipart/form-data">
+	标题：<input type="text" name="title" size="60"><br/>
+	内容：<textarea cols="60" rows="16" name="content"></textarea><br/>
+	类别：<select name="category_id" size="1">
+			<?php
+				include_once("functions/database.php");
+				get_connection();
+				$result_set=mysql_query("select * from category");
+				close_connection();
+				while ($row=mysql_fetch_array($result_set)) {
+				?>
+				<option value="<?php echo $row['category_id'];?>"><?php echo $row['name'];?></option>
+				<?php
+				}
+				?>
+		  </select><br/>
+	附件：<input type="file" name="news_file" size="50">
+		  <input type="hidden" name="MAX_FILE_SIZE" value="10485760">
+		  <br/>
+		  <input type="submit" value="提交">
+		  <input type="reset" value="清空">
+</form>
